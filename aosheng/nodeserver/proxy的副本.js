352b90fa46fb4bcc00f3632afb2452d9bc755e5e
john <johngreat0915@gmail.com>
2018-06-14 12:22:00 +0700

    aosheng

---
+++ aosheng/nodeserver/proxy的副本.js
@@ -0,0 +1,27 @@
+var http = require('http');
+var url=require('url');
+const request = require('superagent')
+
+var server = http.createServer(function (req, response) {
+    response.setHeader("Access-Control-Allow-Origin", "*");
+    response.setHeader("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
+    response.setHeader("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    response.setHeader('Content-Type', 'application/json');
+    var path = url.parse(req.url).path;
+    if(req.method=="OPTIONS") {
+        response.statusCode = 200;
+        response.end();/*让options请求快速返回*/
+    }else if(path.startsWith('/api/')){
+        console.log(path)
+        request
+        .get('http://www.eastasiannews.com:8888'+path)
+        .then(function(res) {
+            response.end(JSON.stringify(res.body));          
+        }).catch(e=>{
+            console.error(e);
+        });
+    }
+   
+});
+server.listen(3000);
+console.log("Server runing at port: " + 3000 + ".");
